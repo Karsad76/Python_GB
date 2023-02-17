2a26696f4fd2ab3821f7429b53abb4d1441b86ed
Sergey Karpov <skarpov@gmail.com>
2023-02-18 00:15:35 +0300

    Edited: sem1 task2 HW code solution added

--- Seminars/seminar1/sem1Task2_HW.py
+++ Seminars/seminar1/sem1Task2_HW.py
@@ -1,1 +1,11 @@
-# Задача 2: Найдите сумму цифр трехзначного числа. 
+# Задача 2: Найдите сумму цифр трехзначного числа.
+
+# geting input data
+num = abs(int(input("Введите любое 3х значное число: ")))
+
+# checking numbers of digits and output the sum of its
+if 99 < abs(num) < 1000:
+    print(int(num % 10 + ((num % 100 - num % 10) / 10) +
+          (num % 1000 - num % 100) / 100))
+else:
+    print("Введенное число не 3х значное")
