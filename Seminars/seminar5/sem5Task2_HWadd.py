65e20f690ca46e5a01b00e818a1cae4069109282
Sergey <skarpov@gmail.com>
2023-03-10 17:50:29 +0300

    Added: solutions for sem5 Task 2 HW add

--- Seminars/seminar5/sem5Task2_HWadd.py
+++ Seminars/seminar5/sem5Task2_HWadd.py
@@ -5,3 +5,21 @@
 Используем операции % //. Операции взятия по индексу применять нельзя.
 Решите через рекурсию. В задании нельзя применять циклы.
 """
+
+
+def calculate_digits(n):
+    count_even = count_odd = 0
+    if n > 1:
+        if n % 10 % 2 == 0:
+            count_even += 1
+        else:
+            count_odd += 1
+        return calculate_digits(n // 10)
+    else:
+        return count_even, count_odd
+
+
+input_num = int(input("Введите натуральное число: "))
+counts = calculate_digits(input_num)
+print(f"Четных цифр - {counts[0]}, нечетных - {counts[1]}")
+
