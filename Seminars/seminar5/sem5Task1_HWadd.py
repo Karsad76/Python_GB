13696ca5bed35835de86f39794e23f405e1b37e3
Sergey <skarpov@gmail.com>
2023-03-10 17:47:21 +0300

    Added: solutions for sem5 Tasks 1,3,5 HW add

--- Seminars/seminar5/sem5Task1_HWadd.py
+++ Seminars/seminar5/sem5Task1_HWadd.py
@@ -10,3 +10,49 @@
 если он ввел 0 в качестве делителя.
 Решите через рекурсию. В задании нельзя применять циклы.
 """
+
+
+def calculator():
+    operation = input("Введите действие: + - * / ** или 0 для выхода: ")
+
+    if operation == "0":
+        print("Конец программы")
+        return
+    else:
+        if operation in "+-*/**":
+            try:
+                arg1 = int(input("Первое число: "))
+                arg2 = int(input("Второе число: "))
+
+                if operation == "+":
+                    print(f"{arg1} + {arg2} = {arg1 + arg2}")
+                    return calculator()
+
+                elif operation == "-":
+                    print(f"{arg1} - {arg2} = {arg1 - arg2}")
+                    return calculator()
+
+                elif operation == "*":
+                    print(f"{arg1} * {arg2} = {arg1 * arg2}")
+                    return calculator()
+
+                elif operation == "**":
+                    print(f"{arg1} ** {arg2} = {arg1 ** arg2}")
+                    return calculator()
+
+                elif operation == "/":
+                    try:
+                        arg1 / arg2
+                    except ZeroDivisionError:
+                        print("На 0 делить нельзя!")
+                    else:
+                        print(f"{arg1} / {arg2} = {arg1 / arg2}")
+                    finally:
+                        return calculator()
+
+            except ValueError:
+                print("Введите число, не строку. Попробуйте еще раз.")
+                return calculator()
+
+
+calculator()
--- Seminars/seminar5/sem5Task5_HWadd.py
+++ Seminars/seminar5/sem5Task5_HWadd.py
@@ -5,3 +5,17 @@
 Решите через рекурсию. В задании нельзя применять циклы.
 Допускается исп-е встроенных ф-ций
 """
+
+
+def print_ascii_table(index):
+    if index < 128:
+        if (index - 32) % 10 == 0:
+            print()
+        print(f"{index:3}-{chr(index)}", end=' ')
+        print_ascii_table(index + 1)
+    else:
+        return
+
+
+start_index = 32
+print_ascii_table(start_index)
