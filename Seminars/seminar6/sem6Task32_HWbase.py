0928b4e7fd54cdde53529def7d2ed2ff931e66ae
Sergey <skarpov@gmail.com>
2023-03-12 18:35:29 +0300

    Added: solutions for sem6 Tasks 30, 32 HW tasks

--- Seminars/seminar6/sem6Task32_HWbase.py
+++ Seminars/seminar6/sem6Task32_HWbase.py
@@ -3,3 +3,17 @@
 принадлежат заданному диапазону (т.е. не меньше заданного минимума и не больше
 заданного максимума)
 """
+
+import random
+
+rand_list = []
+n = 100
+for i in range(n):
+    rand_list.append(random.randint(-100, 100))
+print(rand_list)
+
+min_num = int(input("Введите нижнюю границу диапазона: "))
+max_num = int(input("Введите верхнюю границу диапазона: "))
+for i in rand_list:
+    if min_num <= i <= max_num:
+        print(i, end=' ')
